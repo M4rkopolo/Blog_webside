a79e10dc5987c0f5f6a8af401438e0604b18982e
m4rkopolo <molder.iksx@gmail.com>
2022-06-20 18:37:47 +0200

    commit os environ

--- blog/main/configuration.py
+++ blog/main/configuration.py
@@ -2,8 +2,8 @@ import os
 
 class Config():
     basedir = os.path.abspath(os.path.dirname(__file__))
-    SECRET_KEY = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(basedir, '../../database_file.db')}"
+    SECRET_KEY = os.environ.get("SECRET_KEY")#'8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.poczta.onet.pl'
     MAIL_PORT = 587
